bd0bc319acb2bfdf69011293ac6a8aa17413b576
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-02-17 21:30:11 -0700

    Updates to _turn()
    made it turn the shortest distance

--- Actors/YurisFloatingHead/Player.gd
+++ Actors/YurisFloatingHead/Player.gd
@@ -27,9 +27,9 @@ func _physics_process(delta) -> void:
 	
 	# rotates the sprite to match movement
 	if (move_direction.x > 0.1):
-		_turn(0, 5);
+		_turn(0);
 	elif (move_direction.x < -0.1):
-		_turn(180, 5);
+		_turn(180);
 	
 	# changes the sprite to match movement
 	if (move_direction.z < -0.1):
@@ -61,20 +61,29 @@ func _physics_process(delta) -> void:
 func _on_AnimationPlayer_animation_finished(_anim_name) -> void:
 	_anim_player.play("Idle");
 
-# uses the Tween node to rotate teh sprite smoothly
-func _turn(target_rotation: float, dead_zone: float) -> void:
+# uses the Tween node to rotate the sprite smoothly
+func _turn(target_rotation: float, dead_zone: float = 5.0) -> void:
 	var current_rotation: Vector3 = _sprite_3d.transform.basis.get_euler();
 	var distance_to_rotate: float = abs(target_rotation - rad2deg(current_rotation.y));
 	
+	# cancels the rotation if it is within the dead zone
 	if (distance_to_rotate < dead_zone || abs(distance_to_rotate - 360) < dead_zone):
 		return;
 	
+	# if it would be quicker, rotates the other way
+	if (distance_to_rotate > 180):
+		target_rotation -= 360;
+		distance_to_rotate = abs(target_rotation - rad2deg(current_rotation.y));
+	
+	# calculates the duration of the tween to maintain consistent rotational velocity
 	var duration: float = full_turn_time * (distance_to_rotate / 360);
 	
+	# applies the tween
 	var target_rotation_vector: Vector3 = Vector3(current_rotation.x, target_rotation, current_rotation.z);
 # warning-ignore:return_value_discarded
 	_tween_node.interpolate_property(_sprite_3d, "rotation_degrees",\
 	null, target_rotation_vector, duration, Tween.TRANS_LINEAR);
+# warning-ignore:return_value_discarded
 	_tween_node.start();
 
 # updates the _grounded boolean
