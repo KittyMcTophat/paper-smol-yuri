45775efda70f2faa10d8d9f6176bc9726d241ffe
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-02-23 22:11:17 -0700

    Fixed Shadow Lag
    - Also added SmolSmolYuri

--- Scripts/Actors/Shadow.gd
+++ Scripts/Actors/Shadow.gd
@@ -3,8 +3,10 @@ extends Spatial
 onready var _raycast : RayCast = $RayCast;
 onready var _sprite3D : Sprite3D = $Sprite3D;
 
-func _process(_delta):
+func _physics_process(delta):
 	if (_raycast.is_colliding()):
+		_raycast.force_raycast_update();
+		
 		_sprite3D.global_transform.origin = _raycast.get_collision_point();
 		_sprite3D.transform.origin.y += 0.01
 		_sprite3D.visible = true;
