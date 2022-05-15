f840e5419eb950298f7926eae7b1f7ddb2897713
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-05-14 20:08:17 -0600

    Wobble Mode Update (Again)
    - Fixed a bug where spamming the wobble mode button would result in a non-smooth transition

--- Scripts/GUI/Wobbler.gd
+++ Scripts/GUI/Wobbler.gd
@@ -5,17 +5,26 @@ class_name Wobbler
 onready var material : ShaderMaterial = $ColorRect.material;
 onready var tween : Tween = $Tween;
 
+var target_wobble : float = 0.0;
+
 func _ready():
-	#$ColorRect.visible = false;
 	set_wobbling_intensity(0.0, 0.0);
 
 func set_wobbling_intensity(amount : float, time : float = 1.0):
 	$ColorRect.visible = true;
 # warning-ignore:return_value_discarded
-	tween.interpolate_property(material, "shader_param/max_pixel_offset", null, amount, time);
+	tween.stop_all();
+# warning-ignore:return_value_discarded
+	tween.interpolate_property(material, "shader_param/max_pixel_offset",\
+	material.get_shader_param("max_pixel_offset"), amount, time);
 # warning-ignore:return_value_discarded
 	tween.start();
+	target_wobble = amount;
 
 func _on_Tween_tween_completed(_object, _key):
-	if (material.get_shader_param("max_pixel_offset") == 0):
+	material.set("shader_param/max_pixel_offset", target_wobble);
+	if (target_wobble == 0.0):
 		$ColorRect.visible = false;
+		material.set("shader_param/max_pixel_offset", 0.0);
+	# warning-ignore:return_value_discarded
+		tween.stop_all();
