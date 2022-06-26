b881896b463fcfe18f94f2602d6bec01bdb3037f
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-06-26 10:43:02 -0600

    Smol Changes (See Description)
    - Hid the Sprite3D inside the Overworld King Bowling Cube
    - Ignored the error in InputDisplay.gd

--- Scripts/InputDisplay.gd
+++ Scripts/InputDisplay.gd
@@ -5,6 +5,7 @@ export var is_keyboard : bool = true;
 func _ready():
 	_update_visibility();
 	
+# warning-ignore:return_value_discarded
 	Global.connect("controller_state_changed", self, "_update_visibility");
 
 func _update_visibility():
